e9d57e532486db55af141da24f4bdb8ddd35b4b5
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-04-17 19:24:17 +0700

    memasukan panjang elemen

--- Merge.cpp
+++ Merge.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-
+// create main array & temporary array
+int arr[20], B[20];
+// n is array input size
+int n;
 
 void input()
 {
     while (true)
     {
-        cout << "Masukkan Panjang element array : ";
-        cin >> n;
-
-        if (n <= 20)
+       
         {
             break;
         }
