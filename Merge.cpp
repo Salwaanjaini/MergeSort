8d10e7994039adab38cb9d0716d7f0bd37d75972
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-04-17 19:31:49 +0700

    input is elemen

--- Merge.cpp
+++ Merge.cpp
@@ -29,7 +29,10 @@ void input()
         }
     }
 
-    
+    cout << "\n-----------------------------" << endl;
+    cout << "\nInputkan Isi elemnt array" << endl;
+    cout << "\n-----------------------------" << endl;
+
     for (int i = 0; i < n; i++)
     {
         cout << "Array index ke- " << i << " : ";
